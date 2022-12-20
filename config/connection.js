1d29ea36df5f3728e5a2724115c4daf0d0337229
Naser Suleiman <nasersuleiman421@gmail.com>
2022-12-20 07:52:25 -0800

    trying to set up sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,16 @@
 const Sequelize = require('sequelize');
 
+// Enable access to .env variables
+require('dotenv').config();
+
+// Use environment variables to connect to database
+const sequelize = new Sequelize(
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    // not sure what port to use or select -naser
+    port: 
+  }
+);
+
+module.exports = sequelize;
