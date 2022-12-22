ded3958ea3c5bf131fdf9ff108e64b796b303f55
SeanBristol-Lee <redgreyblu@sbcglobal.net>
2022-12-21 22:11:30 -0800

    add handlebars routes

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
