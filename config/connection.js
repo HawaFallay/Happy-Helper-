2b28731801e0f465b9b77c50b12f82f6216a9dd0
SeanBristol-Lee <redgreyblu@sbcglobal.net>
2022-12-21 22:39:54 -0800

    add env variables in connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,13 @@ const Sequelize = require('sequelize');
 
 // Enable access to .env variables
 require('dotenv').config();
+let sequelize;
 
 // Use environment variables to connect to database
-const sequelize = new Sequelize(
+sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     host: 'localhost',
     dialect: 'mysql',
