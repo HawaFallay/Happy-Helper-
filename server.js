2cdaf9507da30db54844109ea214e334d94f3c6b
Naser Suleiman <nasersuleiman421@gmail.com>
2022-12-22 19:50:44 -0800

    server edits

--- server.js
+++ server.js
@@ -31,3 +31,15 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening on: http://localhost:' + PORT));
 });
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
