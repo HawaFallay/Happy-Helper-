b0ebb69c30683b880a75b9da6a3f7cff03c3290e
Naser Suleiman <nasersuleiman421@gmail.com>
2022-12-23 16:27:54 -0800

    updating task routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 const landingPageRoutes = require('./landingPageRoutes')
 const registerRoutes = require('./registerRoutes')
+const tasksRoutes = require('./tasksRoutes')
 
 router.use('/', homeRoutes);
 router.use('/landingpage',landingPageRoutes)
 router.use('/registerpage',registerRoutes)
+router.use('/clientpage',tasksRoutes)
 module.exports = router;
--- controllers/tasksRoutes.js
+++ controllers/tasksRoutes.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const sequelize = require('./config/connection');
-const Tasks = require('../models/tasks');
+//const sequelize = require('./config/connection');
+const Tasks = require('../../models/tasks');
 
-router.get('/', async (req, res)=>
+router.post('/', (req, res) => {
+    //use sequelize create method to add row to the table
+    Tasks.create({
+        id: req.body.id,
+        task: req.body.task,
+        task_details: req.body.task_details,
+        task_time: req.body.task_time,
+        status_id: req.body.status_id
+    })
+    .then((newTask) => {
+        //send newly created row as JSON
+        res.json(newTask): 
+    })
+    .catch((err) =>{
+        res.json(err);
+
+    });
+});
+
+
+
+
+
+
+/*router.post('/', async (req, res)=>
     Tasks.findAll()
     .then(tasks => {
         console.log(tasks)
         res.sendStatus(200);
     })
     .catch(err => console.log(err))
-);
+);*/
 module.exports = router;
