7fea12d116236fe9bcf760155aa27937cffe7afc
SeanBristol-Lee <seanbristol81@gmail.com>
2022-12-27 22:20:03 -0800

    add role route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const clientRoutes = require('./clientRoutes');
+const roleRoutes = require('./roleRoutes');
 const landingPageRoutes = require('./landingPageRoutes');
 const registerRoutes = require('./registerRoutes');
 const tasksRoutes = require('./tasksRoutes');
 
 router.use('/clients', clientRoutes);
+router.use('/roles', roleRoutes);
 router.use('/registerpage', registerRoutes);
 router.use('/landingpage', landingPageRoutes);
 
